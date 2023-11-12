a6ec18ce4de4731c768bdb40462ff4b596e1f385
euun1 <daeuun1@gmail.com>
2023-11-12 22:17:33 +0900

    5

--- main.c
+++ main.c
@@ -5,11 +5,30 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char str[30]="happy C programming";
-	
-	int strLength = strlen(str);
-	
-	printf("문자열 \"%s\"의 길이 : %i", str, strLength);
+	FILE *filePointer;
+    
+    filePointer = fopen("sample.txt", "w");
+    
+    if (filePointer == NULL) {
+        printf("File not accessible.\n");
+        return 1;
+    }
+
+    char word1[50], word2[50], word3[50];
+    
+    printf("input a word: ");
+    scanf("%s", word1);
+    
+    printf("input a word: ");
+    scanf("%s", word2);
+    
+    printf("input a word: ");
+    scanf("%s", word3);
+
+    fprintf(filePointer, "%s %s %s", word1, word2, word3);
+
+    fclose(filePointer);
+    
 	system("PAUSE");
 	return 0;
 }
